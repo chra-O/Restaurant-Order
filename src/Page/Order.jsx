e0420043292c20dabe6e335e9d904c6801ed3c24
Chra <chra.osman99@gmail.com>
2022-02-01 21:40:15 +0300

    adding orser to firbase

--- src/Page/Order.jsx
+++ src/Page/Order.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import NavBar from "../Component/NavBar";
 import { useSelector } from "react-redux";
 export default function Order() {
-  // const [TotalPrice , setTotalPrice] = useState("");
+  //https://test-44c0b-default-rtdb.firebaseio.com/
   const orderFood = useSelector((state) => state.food.value);
   const totalPric = [];
   let tottal = 0;
@@ -12,8 +12,13 @@ export default function Order() {
   totalPric.map((product) => {
     tottal += product;
   });
+  useEffect(() => {
+    fetch("https://test-44c0b-default-rtdb.firebaseio.com/order.json", {
+      method: "PUT",
+      body: JSON.stringify(orderFood),
+    }).catch((e) => console.log(e));
+  });
 
-  console.log(tottal);
   return (
     <>
       <NavBar />
@@ -26,7 +31,7 @@ export default function Order() {
           {orderFood.map((product, index) => {
             return (
               <div
-                className="bg-gray-50 w-72 h-72 m-10  grid grid-rows-2 rounded-3xl"
+                className="bg-gray-50 w-72 h-80 m-8  grid grid-rows-2 rounded-3xl"
                 key={index}
               >
                 <div className="flex justify-center ">
@@ -42,9 +47,17 @@ export default function Order() {
                     {product.catigory}
                   </div>
                   <div className=" ml-3 font-mono "> type : {product.name}</div>
-                  <div className=" grid grid-cols-2 font-mono">
+                  <div className=" grid grid-cols-2 font-mono text-sm ">
                     <p className="ml-3">Price : {product.price}$</p>
-                    <div></div>
+                    <div>
+                      Qualrty :
+                      <button className="mr-10 w-5 h-5 mt-2 ml-9">
+                        <img src="https://cdn-icons-png.flaticon.com/128/25/25678.png"></img>
+                      </button>
+                      <button className="mr-2 w-5 h-5 mt-2">
+                        <img src="https://cdn-icons.flaticon.com/png/128/4655/premium/4655094.png?token=exp=1643741038~hmac=1a92c750766df0f1e4c3ac21436e5320"></img>
+                      </button>
+                    </div>{" "}
                   </div>
                 </div>
               </div>
