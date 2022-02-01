4730af066f8afd01414a13481558dc262e5399d2
Chra <chra.osman99@gmail.com>
2022-02-01 21:10:52 +0300

    addint tottal price

--- src/Page/Order.jsx
+++ src/Page/Order.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../Component/NavBar";
 import { useSelector } from "react-redux";
 export default function Order() {
+  // const [TotalPrice , setTotalPrice] = useState("");
   const orderFood = useSelector((state) => state.food.value);
+  const totalPric = [];
+  let tottal = 0;
+  orderFood.map((product) => {
+    totalPric.push(product.price);
+  });
+  totalPric.map((product) => {
+    tottal += product;
+  });
 
+  console.log(tottal);
   return (
     <>
       <NavBar />
-      <div className="flex justify-center ">
+
+      <div className="flex justify-end  ">
         <div className=" grid sm:grid-cols-1  md:grid-cols-2 lg:grid-cols-4     ">
-          {orderFood.map((product) => {
+          <div className="mt-40 font-bold text-2xl w-72 h-32 bg-gray-50  rounded-3xl">
+            <p className="mt-12 ml-5">Total price : {tottal}$ </p>
+          </div>
+          {orderFood.map((product, index) => {
             return (
               <div
                 className="bg-gray-50 w-72 h-72 m-10  grid grid-rows-2 rounded-3xl"
-                key={product.id}
+                key={index}
               >
                 <div className="flex justify-center ">
                   <img
